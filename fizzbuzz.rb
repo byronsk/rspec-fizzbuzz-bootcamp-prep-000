1dd31af623bc5c61875c3317caa7f1c1b9e6e792
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-20 21:08:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+  return "FizzBuzz"
+  
   if int % 3 == 0 
   return "Fizz"
 end
   
  if int % 5 == 0 
   return  "Buzz"
-end
-
-if int % 3 == 0 && int % 5 == 0
-  return "FizzBuzz"
  end
 end
